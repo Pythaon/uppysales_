8e50a14c37a14de29678e9bcab20e654721bc568
CelineChg <79990186+CelineChg@users.noreply.github.com>
2021-07-13 15:11:28 +0200

    Update test_clust.py

--- test_clust.py
+++ test_clust.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 12 19:52:38 2021
-
-@author: Utilisateur
+Created on Thu Jul  8 19:18:48 2021
+@author: Céline & Thao 
 """
 
 import pandas as pd
@@ -18,49 +17,34 @@ import io
 from io import StringIO
 import os
 
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+from sklearn import neighbors
+from sklearn import datasets
+from scipy.spatial.distance import cdist
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import silhouette_score
+
 ##------- ALL PAGE 
-st.set_page_config(page_title="UpPySales App",page_icon="🎯",layout="wide",initial_sidebar_state="expanded")
+st.set_page_config(page_title="UpPySales App",page_icon="🎯",layout="centered",initial_sidebar_state="expanded")
 
 page = st.sidebar.radio(label="Menu", options = ['1️⃣ Présentation',  '2️⃣ Segmentation visiteurs', '3️⃣ Clustering'])
 
 ### LOGO
-
+@st.cache
+def img():
+    urllogo = "https://raw.githubusercontent.com/Pythaon/uppysales_/main/Graph_coude.PNG"
+    image = Image.open(requests.get(urllogo, stream=True).raw)
+    return image
     
+image = img()
     
-st.title ("Clustering")
+st.image(image, width=None)
 
-
-st.markdown("""
-            Nous allons tester les modèles suivants:
-                
-                
-                """)
-            
-models = ['Kmeans', 'Clustering Mixte kmeans & ACH']
-          
-choix_modele = st.radio("", options=models)
-
-
-
-if choix_modele ==models[0]:
-    
-    #@st.cache
-    #def img():
-     #   urllogo = "https://raw.githubusercontent.com/Pythaon/uppysales_/main/Graph_coude.png"
-      #  image = Image.open(requests.get(urllogo, stream=True).raw)
-       # return image
-    
-    #image = img()
-
-    @st.cache
-    def img():
-        urllogo = "https://raw.githubusercontent.com/Pythaon/uppysales_/main/Graph_coude.PNG"
-        image = Image.open(requests.get(urllogo, stream=True).raw)
-        return image
-    
-    image = img()
-    
-    
-    st.write("""
-             Le coude n'est pas très franc mais apparait autour du nombre de cluster = 4
-             """)
+st.title("""**Analyse de l'activité de e-commerce**""")
